bc07d1ce2e06d9666dc8d881e9d7255bdd219b03
Icelogia <35977372+Icelogia@users.noreply.github.com>
2021-04-21 20:48:29 +0200

    Soundtrack update

---
+++ Assets/Scripts/Menu/PlayMusic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMusic : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource = null;
+
+    [SerializeField] private AudioClip[] audioClips;
+
+    private void Start()
+    {
+        AudioClip clip = DrawMusicClip();
+        PlaySpecificMusic(clip);
+    }
+
+    private AudioClip DrawMusicClip()
+    {
+        int musicIndex = Random.Range(0, audioClips.Length);
+        return audioClips[musicIndex];
+    }
+
+    private void PlaySpecificMusic(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        StartCoroutine(PlayNextMusicAfter(audioClip.length));
+    }
+
+    private IEnumerator PlayNextMusicAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        AudioClip clip = DrawMusicClip();
+        PlaySpecificMusic(clip);
+    }
+}
